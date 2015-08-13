a64e1f46618494c26c6a99fa69997a2466383f3f
naveenreddyin <naveenreddyin@gmail.com>
2015-08-13 21:44:41 +0200

    initial commit

--- app/src/main/java/core/parent/core/MainActivity.java
+++ app/src/main/java/core/parent/core/MainActivity.java
@@ -1,9 +1,13 @@
 package core.parent.core;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,40 +15,45 @@ import android.view.View;
 import com.parent.parent.ParentActivity;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseAcitivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     /** handles the click button event of parent */
     public void clickParentEvent(View view){
         Intent intent = new Intent(this, ParentActivity.class);
         startActivity(intent);
+    }
+
+    /** handles the click button event of kid */
+    public void clickKidEvent(View view){
+        setSharedPreferencesToTrue(0);
+        Intent intent = new Intent(this, ParentActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void setSharedPreferencesToTrue(int which){
+
+        SharedPreferences prefs = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor edit = prefs.edit();
+        Log.i("MainActivity", "_______________________"+which);
+        if(which == 0){
+            Log.i("MainActivity", "_______________________ comig inside 0 ");
+            edit.putBoolean(getString(R.string.is_parent), true);
+            edit.apply();
+        }else{
+            Log.i("MainActivity", "_______________________ comig inside 1 ");
+
+            edit.putBoolean(getString(R.string.is_kid), true);
+            edit.commit();
+        }
+
     }
 }
